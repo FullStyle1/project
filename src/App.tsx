f2f4d50506e0cee0537c8ea655accfc18f539bee
FullStyle1 <drksh2015@gmail.com>
2025-01-30 21:45:44 -0600

    das

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { UserPlus, Search, PencilLine, Trash2, X } from 'lucide-react';
-import logoImage from './dist/assets/Logotipo_UTCH.jpg';
+import logoImage from 'Assets/Logotipo_UTCH.jpg';
 
 interface Teacher {
   id: number;
